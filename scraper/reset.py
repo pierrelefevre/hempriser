6633e8b39db3071904916afae9fd6c4a65444d19
Pierre Le Fevre <hello@pierrelf.com>
2023-12-05 16:38:32 +0100

    Update reset.py to set status to "pending" for collections that meet the condition

--- scraper/reset.py
+++ scraper/reset.py
@@ -10,6 +10,7 @@ reset = {
 
 for collection_name, should_reset in reset.items():
     if should_reset and collection_name in db.c:
+        fil = {"status": {"$ne": "pending"}}
         write = {"status": "pending"}
 
-        db.c[collection_name].update_many({}, {"$set": write})
+        db.c[collection_name].update_many(fil, {"$set": write})
